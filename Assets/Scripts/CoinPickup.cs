eacdc9a54388fa702cc987269682581865e6e950
Lionel Pointet <lionel.pointet@gmail.com>
2015-08-08 14:29:36 +0200

    Implémentation des Pickup génériques et des CoinPickup pour les pièces, ajout d'un temps de vie pour les Pickup après avoir été ramassés

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickup : Pickup {
+	
+	public int pointToAdd;
+
+	protected override void OnPick() {
+		// Ajouter les points au joueur
+		ScoreManager.AddPoint(pointToAdd);
+	}
+}
--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -2,13 +2,38 @@
 using System.Collections;
 
 public class Pickup : MonoBehaviour {
-
-	public int pointToAdd;
-
+	
+	public float lifeTime = 0;
+	private Renderer renderer;
+	
+	void Awake() {
+		renderer = GetComponent<Renderer>();
+	}
+	
+	protected virtual void OnPick() {
+		// Que faut-il faire lorsque cet objet a été ramassé ?
+	}
+	
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.name == "Heros") {
-			ScoreManager.AddPoint(pointToAdd);
-			gameObject.SetActive(false);
+			if( lifeTime > 0 ) {
+				Hide();
+				StartCoroutine( Despawn() );
+			}
+			else {
+				gameObject.SetActive(false);
+			}
+			
+			OnPick();
 		}
+	}
+	
+	private void Hide() {
+		renderer.enabled = false;
+	}
+
+	private IEnumerator Despawn() {
+		yield return new WaitForSeconds( lifeTime );
+		gameObject.SetActive( false );
 	}
 }
