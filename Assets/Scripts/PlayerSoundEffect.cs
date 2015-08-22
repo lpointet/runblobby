f15b3f26c9ad7c8e86aa27039726df7e5817c5ba
Angel <k_mikazoo@yahoo.fr>
2015-08-22 14:34:01 +0200

    Sons pour le joueur 1

---
+++ Assets/Scripts/PlayerSoundEffect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSoundEffect : MonoBehaviour {
+
+	private AudioSource soundSource;
+	public AudioClip jumpSfx;
+	public AudioClip moveSfx;
+
+	void Awake() {
+		soundSource = GetComponent<AudioSource> ();
+	}
+	
+	private void FootStepSound() {
+		soundSource.Stop ();
+		soundSource.volume = 0.2f;
+		soundSource.PlayOneShot (moveSfx);
+	}
+	
+	private void JumpSound() {
+		soundSource.Stop ();
+		soundSource.volume = 0.4f;
+		soundSource.PlayOneShot (jumpSfx);
+	}
+}
