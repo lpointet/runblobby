662906c80e208e3aa32a9808c7d425d3d644a822
Lionel Pointet <lionel.pointet@gmail.com>
2015-08-23 23:22:33 +0200

    Démarrage du FlyPickup

---
+++ Assets/Scripts/FlyPickup.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyPickup : Pickup {
+	private float initialGravityScale;
+	private Rigidbody2D playerBody;
+
+	void Start() {
+		playerBody = LevelManager.getPlayer().GetComponent<Rigidbody2D>();
+
+		if( null == playerBody ) {
+			Debug.LogError( "Pas de Rigidbody2D sur le joueur ?!?" );
+		}
+
+		initialGravityScale = playerBody.gravityScale;
+	}
+
+	protected override void OnPick() {
+		// Abaisser la gravité
+		playerBody.gravityScale = 1.5f;
+
+		// TODO: Faire décoller le joueur
+
+		// TODO: Faire en sorte que le nombre de sauts soit illimité
+	}
+
+	protected override void OnDespawn() {
+		// Remettre la gravité
+		playerBody.gravityScale = initialGravityScale;
+
+		// TODO: Remettre une limite au nombre de sauts ?
+	}
+}
