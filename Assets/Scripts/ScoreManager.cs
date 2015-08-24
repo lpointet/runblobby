579a3435feb74a46b5cc8b22e3c222ffa4cd40e3
Lionel Pointet <lionel.pointet@gmail.com>
2015-08-25 00:14:08 +0200

    Mise en place d'un système de multiplication des points par type

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
+	enum Types {
+		All,
+		Coin,
+		Enemy
+	};
+
 	Text scoreText;
 
 	private static int score;
+	private static Dictionary<int, int> multipliers = new Dictionary<int, int>();
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +26,26 @@ public class ScoreManager : MonoBehaviour {
 		scoreText.text = score + "$";
 	}
 
-	public static void AddPoint(int numberPoint){
+	public static void AddPoint(int numberPoint, int type = (int)Types.All){
+		int multiplier;
+		if( multipliers.TryGetValue( type, out multiplier ) ) {
+			numberPoint*= multiplier;
+		}
 		score += numberPoint;
+	}
+
+	public static void AddMultiplier( int multiplier, int type = (int)Types.All ) {
+		int initial;
+		if( multipliers.TryGetValue( type, out initial ) ) {
+			multiplier = Mathf.Max( multiplier, initial );
+			multipliers.Remove( type );
+		}
+
+		multipliers.Add( type, multiplier );
+	}
+
+	public static void RemoveMultiplier( int type = (int)Types.All ) {
+		multipliers.Remove( type );
 	}
 
 	public static void Reset(){
