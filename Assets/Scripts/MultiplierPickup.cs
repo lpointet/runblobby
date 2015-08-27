db065d1a078d615b3acb9a690e5338b55b16a7bb
Lionel Pointet <lionel.pointet@gmail.com>
2015-08-27 23:32:41 +0200

    Ajout du pickup d'ajout de multiplicateur

---
+++ Assets/Scripts/MultiplierPickup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiplierPickup : Pickup {
+
+	public int multiplier;
+	public ScoreManager.Types type;
+
+	protected override void OnPick() {
+		base.OnPick();
+
+		ScoreManager.AddMultiplier( multiplier, type, lifeTime );
+	}
+
+	protected override void OnDespawn() {
+		base.OnDespawn();
+
+		ScoreManager.RemoveMultiplier( type );
+	}
+
+}
