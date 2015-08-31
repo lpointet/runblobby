9239bb0009ba11a721dd01008e02e8ed71472f9a
Angel <k_mikazoo@yahoo.fr>
2015-08-31 19:24:45 +0200

    Blocage du background si le joueur est mort

--- Assets/Scripts/BackgroundScrolling.cs
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -7,8 +7,11 @@ public class BackgroundScrolling : MonoBehaviour {
 	private float ratioVitesse;
 	private Material myMaterial;
 
+	private PlayerController player;
+
 	void Start() {
 		myMaterial = GetComponent<Renderer> ().material;
+		player = LevelManager.getPlayer ();
 
 		// Adapater l'échelle à la taille de l'écran pour qu'on voit tout à chaque fois
 		//transform.localScale = new Vector2 (Camera.main.orthographicSize * Camera.main.aspect * 2, Camera.main.orthographicSize * 2);
@@ -20,11 +23,13 @@ public class BackgroundScrolling : MonoBehaviour {
 	}
 
 	void Update () {
-		scrollSpeed = LevelManager.getPlayer ().GetMoveSpeed () / ratioVitesse;
+		if (!player.IsDead ()) {
+			scrollSpeed = LevelManager.getPlayer ().GetMoveSpeed () / ratioVitesse;
 
-		float x = Mathf.Repeat (Time.time * scrollSpeed, 1);
-		Vector2 offset = new Vector2 (x, myMaterial.mainTextureOffset.y);
-		myMaterial.mainTextureOffset = offset;
+			float x = Mathf.Repeat (Time.time * scrollSpeed, 1);
+			Vector2 offset = new Vector2 (x, myMaterial.mainTextureOffset.y);
+			myMaterial.mainTextureOffset = offset;
+		}
 	}
 
 }
