71904c6968bedb66641d107e5ccc050ebabbebc5
Angel <k_mikazoo@yahoo.fr>
2015-09-05 21:05:55 +0200

    Script des plateformes qui tombent

---
+++ Assets/Scripts/Fallen_Plateform.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fallen_Plateform : MonoBehaviour {
+
+	private PlayerController player;
+
+	private Transform pointFallen;
+	public Transform fallenObject; // Objet qui doit tomber, caché au début mais existant
+	public float delay; // Délai de chute de la plateforme
+	public float chuteMultiplier = 2f;
+
+	private bool blockAppeared = false;
+
+	void Start () {
+		player = LevelManager.getPlayer ();
+		pointFallen = transform;
+	}
+
+	void Update () {
+		if (blockAppeared) {
+			fallenObject.Translate(Vector3.down * Time.deltaTime * player.GetMoveSpeed() * chuteMultiplier);
+
+			if (fallenObject.position.y - 1 <= pointFallen.position.y) {
+				blockAppeared = false; // on arrete le mouvement de chute
+				fallenObject.position = new Vector2(pointFallen.position.x, pointFallen.position.y + 1);
+			}
+		}
+	}
+
+	void OnBecameVisible() {
+		// On le place au-dessus du point d'impact et hors de l'écran
+		float heightOverScreen = (1 + Camera.main.GetComponent<CameraManager>().yOffsetPourcentage) * Camera.main.orthographicSize + Mathf.Abs(pointFallen.position.y);
+		fallenObject.position = new Vector2(pointFallen.position.x, heightOverScreen);
+
+		// On attend la distance que l'on souhaite adaptée à la vitesse du joueur
+		Invoke ("TomberObjet", delay * (player.GetInitialMoveSpeed () / player.GetMoveSpeed ()));
+	}
+
+	private void TomberObjet() {
+		blockAppeared = true;
+	}
+}
