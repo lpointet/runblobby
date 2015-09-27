067285397b4af510db76991bd823f5d7b7a8850a
Lionel Pointet <lionel.pointet@gmail.com>
2015-09-27 20:27:47 +0200

    Ajout du pickup de soin

---
+++ Assets/Scripts/Pickup/HealPickup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealPickup : Pickup {
+
+	public int heal;
+
+	protected override void OnPick() {
+		base.OnPick();
+
+		// Ajouter la vie au joueur
+        LevelManager.getPlayer().SetHealthPoint( LevelManager.getPlayer().GetHealthPoint() + heal );
+	}
+}
